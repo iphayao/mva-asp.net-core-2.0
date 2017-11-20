69401f1a225479a3a0acc1da02718ee4627dead7
iphayao <phayaob@gmail.com>
2017-11-20 20:15:00 +0700

    Introduction to the .NET CLI & Templating

---
+++ beginner/01_GettingStarted/myconsoleapp/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace myconsoleapp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Scott!");
+            Console.ReadLine();
+        }
+    }
+}
