d9636920b18e3e660b587df8d2c81d5308442f17
iphayao <phayaob@gmail.com>
2017-11-29 20:44:04 +0700

    Tag Helpers

---
+++ intermediate/02_TagHelpers/WebApplication/WebApplication/RepeatTagHelper.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication
+{
+    public class RepeatTagHelper : TagHelper
+    {
+        public int Count { get; set; }
+        public override async Task ProcessAsync(TagHelperContext context, TagHelperOutput output)
+        {
+            for(int i = 0; i < Count; i++)
+            {
+                output.Content.AppendHtml(await output.GetChildContentAsync(useCachedResult: false));
+            }
+        }
+    }
+}
